b52427030e5f76578d9b5c966be588cf86327ab0
naseeftp <naseeftp35@gmail.com>
2026-04-01 16:02:32 +0530

    after chaging error handle to handle zod error message

--- frontend/src/utils/error.handle.ts
+++ frontend/src/utils/error.handle.ts
@@ -1,14 +1,21 @@
 import { isAxiosError } from "axios";
 
-export const apiErrorHandler=(error:unknown,defaulMessage:string)=>{
- if(isAxiosError(error)){
-   return{
-        success:false,
-        message:error.response?.data?.message||defaulMessage
+export const apiErrorHandler = (error: unknown, defaultMessage: string) => {
+  if (isAxiosError(error)) {
+    const responseData = error.response?.data;
+    if (responseData?.errors && Array.isArray(responseData.errors) && responseData.errors.length > 0) {
+     return {
+        success: false,
+        message: responseData.errors[0].message,
+      };
     }
- }
- return{
-    success:false,
-    message:error  instanceof Error?error.message:defaulMessage
- }
-}
+    return {
+      success: false,
+      message: responseData?.message || defaultMessage,
+    };
+  }
+  return {
+    success: false,
+    message: error instanceof Error ? error.message : defaultMessage,
+  };
+};
