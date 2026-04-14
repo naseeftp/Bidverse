eff0c961b04ca4b9ed8e8a55cfa1997e0f7e5fac
naseeftp <naseeftp35@gmail.com>
2026-04-14 07:07:16 +0530

    after includde admin in protected route

--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -6,16 +6,20 @@ interface ProtectedRouteProps {
 }
 
 const ProtectedRoute = ({ allowedRoles }: ProtectedRouteProps) => {
- 
+
   const { isAuthenticated, user, loading } = useAppSelector((state) => state.auth);
 
   if (loading) return <div className="flex h-screen items-center justify-center">Loading...</div>;
-
   if (!isAuthenticated) {
     return <Navigate to="/" replace />;
   }
   if (allowedRoles && user && !allowedRoles.includes(user.role)) {
-    const redirectPath = user.role === 'tenant' ? "/tenant/dashboard" : "/home";
+    let redirectPath = "/home";
+    if (user.role === 'tenant') {
+      redirectPath = "/tenant/dashboard";
+    } else if (user.role === 'admin') {
+      redirectPath = "/admin/dashboard";
+    }
     return <Navigate to={redirectPath} replace />;
   }
 
