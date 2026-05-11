3805dbd70094cfb399fd2b355336b6d2e53f3cb2
naseeftp <naseeftp35@gmail.com>
2026-05-11 12:47:04 +0530

    feat(address): add validation for address management

--- backend/src/services/implementations/AddressService.ts
+++ backend/src/services/implementations/AddressService.ts
@@ -2,7 +2,7 @@ import { IAddressService } from "../interface/IAddress.service";
 import { IAddressRepository } from "../../repositories/interfaces/IAddress.repository";
 import { ILoggerService } from "../interface/ILogger.service";
 import { CreateAddressDTO, AddressResponseDTO, deleteAddressDTO } from "../../dtos/user.dto/address.dto";
-import { AppError, NotFoundError,ForbiddenError} from "../../errors/AppError";
+import { AppError, NotFoundError, ForbiddenError } from "../../errors/AppError";
 import { Types } from 'mongoose'
 import { AddressMapper } from "../../mappers/address.mapper";
 import { AddressLabel, MESSAGES } from "../../constants/constants";
@@ -57,34 +57,58 @@ export class AddressService implements IAddressService {
   }
 
   async deleteAddress(userId: string, addressId: string, data: deleteAddressDTO): Promise<AddressResponseDTO | null> {
+    const addressToDelete = await this._addressRepo.findById(addressId)
+    if (!addressToDelete) {
+      throw new NotFoundError(MESSAGES.ADDRES_NOT_FOUND)
+    }
+    if (addressToDelete.userId.toString() != userId) {
+      throw new ForbiddenError('You are not allowed to delete address')
+    }
 
     const filter = {
       _id: addressId,
       userId: userId,
     }
-    const deletedAddress = await this._addressRepo.updateByFilter(filter, data)
-    if (!deletedAddress) {
-      throw new NotFoundError(MESSAGES.ADDRES_NOT_FOUND)
+    const deletedAddress = await this._addressRepo.updateByFilter(filter, data);
+    if (addressToDelete.isDefault) {
+      const remainingAddress = await this._addressRepo.findAllUserAddress(userId, 1, 1);
+      if (remainingAddress.docs.length > 0) {
+        const newDefaultId = remainingAddress.docs[0]._id.toString();
+        await this._addressRepo.updateByFilter({ _id: newDefaultId, userId: userId }, { isDefault: true })
+      }
     }
-    return AddressMapper.toAddressDto(deletedAddress)
+
+    return AddressMapper.toAddressDto(deletedAddress!)
   }
 
   async editAddress(userId: string, addressId: string, data: CreateAddressDTO): Promise<AddressResponseDTO> {
-    const existingAddress=await this._addressRepo.findById(addressId)
-    if(!existingAddress){
+    const existingAddress = await this._addressRepo.findById(addressId)
+    if (!existingAddress) {
       throw new NotFoundError(MESSAGES.ADDRES_NOT_FOUND)
     }
-    if(existingAddress.userId.toString()!==userId){
-     throw new ForbiddenError('"You do not have permission to edit this address"')
+    if (existingAddress.userId.toString() !== userId) {
+      throw new ForbiddenError("You do not have permission to edit this address")
     }
-    if(data.isDefault){
+    const isDuplicate= await this._addressRepo.findDuplicate(userId,data.fullAddress,data.city,data.pincode)
+    if(isDuplicate&&isDuplicate._id.toString()!==addressId){
+      throw new AppError('An identical address already exists.');
+    }
+    let finalIsDefault=data.isDefault
+    const totalCounts=await this._addressRepo.countByUserId(userId)
+    if(totalCounts==1){
+      finalIsDefault=true
+    }
+    if(existingAddress.isDefault&&finalIsDefault==false&&totalCounts>1){
+      throw new AppError("Please set another address as default before removing this one.")
+    }
+    if (finalIsDefault) {
       await this._addressRepo.unsetDefaults(userId)
     }
     const filter = {
       _id: addressId,
       userId: userId
     }
-    const editedAddress = await this._addressRepo.updateByFilter(filter, data);
+    const editedAddress = await this._addressRepo.updateByFilter(filter,{...data,isDefault:finalIsDefault});
     return AddressMapper.toAddressDto(editedAddress!)
   }
 }
