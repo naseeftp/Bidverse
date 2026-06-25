393a5e244de26b6c6714592b26f9d705b1280c30
naseeftp <naseeftp35@gmail.com>
2026-06-25 20:28:43 +0530

    feat(chat) listing specific user conversations started

--- backend/src/services/interface/IChat.service.ts
+++ backend/src/services/interface/IChat.service.ts
@@ -1,5 +1,7 @@
 import { ConversationDTO } from "../../dtos/user.dto/chat.dto";
+import { IGenericPaginatedResposnse } from "../../types/response.type";
 
 export interface IChatService{
  getOrCreateConversation(participants:{userId:string;role:string}[]):Promise<ConversationDTO>
+ getUserConversation(userId:string):Promise<IGenericPaginatedResposnse<ConversationDTO>>
 }
